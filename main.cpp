dd40dfe8a12f9b6c7939a8f1d8f96eecddd0da45
WingTangWong <wingedpower@gmail.com>
2020-03-21 12:33:21 -0700

    Adding 10 UDTs. posting for OK before continuing

--- main.cpp
+++ main.cpp
@@ -14,7 +14,11 @@ Video:  Chapter 2 Part 5
     Ping me in Slack to review this step.
     Wait for me to review before proceeding with step 2.
 ************************
-  
+
+
+   * WingTangWong - done, added in the space below
+
+
  2) define your struct for each of your 10 types. 
     Copy your 3+5 comments into your struct body.
     comment them out.
@@ -62,6 +66,141 @@ Car Wash
     2) charge customer
     3) detail the car interior
  */
+
+
+/* WingTangWong's UDT(s)
+  
+   (1)Drive Thru Coffee Stand 
+   5 properties:
+      1) physical attributes (location, dimensions, color, size, orientation)
+      2) open for business or not (boolean or array of times to be checked against)
+      3) stores inventory of consumables 
+      4) stores financial transactions
+      5) stores daily business metrics
+   3 things it can do:
+      1) take orders
+      2) produce goods for order
+      3) accept payment for goods
+
+   (2)Microwave
+   5 properties:
+      1) wattage
+      2) plugged in or not
+      3) active or not
+      4) current settings
+      5) time remaining
+   3 things it can do:
+      1) open/close door
+      2) set time/power levels
+      3) start/stop
+
+   (3)Smart Power Strip
+   5 properties:
+      1) network address(ip address/netmask/gateway/dns)
+      2) list of individual socket states (on/off) 
+      3) stored history of power consumption per socket 
+      4) stored history of socket state changes
+      5) stored 'uptime' value
+   3 things it can do:
+      1) change individual socket state: on-to-off,off-to-on,toggle/bounce
+      2) perform global socket change: all on, all off, all toggle/bounce
+      3) reconfigure network settings
+
+   (4)Kitchen Droid
+   5 properties:
+      1) Left/Right armature hardware installed: none, or one of several kitchen tool ID(s) assigned/loaded.
+      2) Inventory of alternate hardware(s) swappable. 
+      3) List of activities performable with arm/tools 
+      4) List of ingredients in stock
+      5) Menu of dishes Kitchen Droid can make
+   3 things it can do:
+      1) Prepare Meal From Menu
+      2) Swap tools
+      3) Validate menu choice with available tools, ingredients,and actions available. 
+
+   (5)Spoon
+   5 properties:
+      1) spoon dimensions
+      2) spoon carrying capacity
+      3) spoon contents
+      4) spoon orientation
+      5) spoon temperature 
+   3 things it can do:
+      1) fill with something (scoop attempt, can return a bool of true/false if not scoopable)
+      2) empty contents      (upturn spoon to release contents)
+      3) deliver contents    (move spoon filled with content to something else do transfer contents) 
+
+   (6)Apple Store
+   5 properties:
+      1) Inventory of Apple products
+      2) Location
+      3) Hours of operation
+      4) Number of staff
+      5) Hourly Profit
+   3 things it can do:
+      1) Sell Apple Products To New Customers
+      2) Sell Apple Products To Existing Customers
+      3) Collect Money
+
+   (7)Wild Squirrel
+   5 properties:
+      1) Number of nuts in mouth
+      2) Number of trees visited per hour
+      3) Has Jumped In Baby Carriage in Lifetime (bool)
+      4) Is Sprinting(bool)
+      5) Is Chittering(bool)
+   3 things it can do:
+      1) Place Nut In Mouth
+      2) Sprint to random location (inclusive of baby carriages)
+      3) Chitter for random length of time.
+
+   (8)Dungeon Master 
+   5 properties:
+      1) List of voices and personas capable of enacting
+      2) List of various dice to roll for actions/effects
+      3) List of ideas on how to manipulate the party
+      4) List of documents and reference books on hand
+      5) List of player penalties for being difficult *cough*blue lightning*cough*
+   3 things it can do:
+      1) Enact voices and personas to party
+      2) Request the party "roll for initiative"
+      3) Laugh maniacally
+
+   (9)D&D Party Members
+   5 properties:
+      1) Player character stats/attributes(age, sex, race, armor class, pasive perception, etc)
+      2) Assortment of dice for rolling for actions and effects
+      3) Is a murderhobo (bool) (senselessly killing non-player-characters in game)
+      4) Fearful of Dungeon Master (bool)
+      5) How badly they want to kill "that partymember" (double)
+   3 things it can do:
+      1) Fulfill quests
+      2) Listen to the dungeon master's amazing voices
+      3) Roll for initiative
+
+   (10)Self Checkout Register
+   5 properties:
+      1) has screen for user interface
+      2) has scanner for products
+      3) has scale for bagging and validation of products
+      4) has payment interface
+      5) has receipt printer
+   3 things it can do:
+      1) ring up item
+      2) accept payment
+      3) print receipt
+
+*/
+
+
+
+
+
+
+
+
+
+
 struct CarWash //2)        
 {
     //2) has vacuum cleaners
